d3bf0fda147580e597efd34c06a00db4aca0c7d5
Anes <suhonjicanes96@gmail.com>
2023-03-08 23:11:39 -0600

    uploadfile

--- Web.py
+++ Web.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import functions
 
+uploaded_file = st.file_uploader(
+    "Choose your database", accept_multiple_files=False)
+if uploaded_file is not None:
+    file_name = uploaded_file
+else:
+    file_name = "DatabaseSample.xlsx"
 
 todos = functions.read()
 
